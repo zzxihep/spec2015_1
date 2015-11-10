735a6229ff9c46c1c3e8e776eeef9b2d376718ef
zzxihep <zhngzhxiang@ihep.ac.cn>
2015-11-10 10:42:04 +0800

    creat

---
+++ spec2015.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+${0%/*}/mv_to_spec.py
+cd spec
+${0%/*}/classify_fits.py
+${0%/*}/mv_to_other.py
+${0%/*}/gen_lst.py
+${0%/*}/check_lessing.py
+${0%/*}/check_bad.py
+${0%/*}/check_manual.py
+${0%/*}/gen_zero.py
+${0%/*}/re.py
+cd ..
