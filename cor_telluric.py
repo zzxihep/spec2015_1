c83a0c2e39b6256b92d156e817c0095e7eb10fbb
zzxihep <zhangzx@ihep.ac.cn>
2017-06-26 06:37:39 +0800

    update

--- cor_telluric.py
+++ cor_telluric.py
@@ -1,18 +1,23 @@
-# coding=utf8
 #!/usr/bin/env python
+# coding=utf8
 
 import os
 import sys
+import glob
+import pyfits
 import func
 from pyraf import iraf
 
+
 def to_str(lst, sep=','):
     """
-    convert string list to a sum string, like lst = ['abc.fits', 'def.fits', 'ghi.fits'], return 'abc.fits,def.fits,ghi.fits'
+    convert string list to a sum string,
+    like lst = ['abc.fits', 'def.fits', 'ghi.fits'],
+    return 'abc.fits,def.fits,ghi.fits'
     lst : string list
     type : list
-    sep : separator, default is ','
-    type : char
+    sep : separator, default ','
+    type : string
     return : sum string
     type : string
     """
@@ -23,9 +28,21 @@ def to_str(lst, sep=','):
     ret = ret[:-size]
     return ret
 
-def scopy_cmp(fn, w1 = 'INDEF', w2 = 'INDEF'):
+
+class abc(self):
+    def println(self):
+        print "Hello World"
+
+    def random(self):
+        pass
+
+
+def scopy_cmp(fn, w1='INDEF', w2='INDEF'):
     """
-    copy part of a spectrum, and generate a new spectrum fits file. if a same name file already exist, this function will delete the old one. if the fits fn have more than one apertures, this function will just extract the last aperture.
+    copy part of a spectrum, and generate a new spectrum fits file.
+    if a same name file already exist, this function will delete the old.
+    if the fits fn have more than one apertures,
+    this function will just extract the last aperture.
     fn : fits name
     type : string
     w1 : start wavelength, default='INDEF'
@@ -35,20 +52,22 @@ def scopy_cmp(fn, w1 = 'INDEF', w2 = 'INDEF'):
     return : out put file name
     type : string
     """
-    nax2 = pyfits.getval(fn, keyword = 'NAXIS2')
+    nax2 = pyfits.getval(fn, keyword='NAXIS2')
     outname = str(w1)+'_'+str(w2)+'_'+fn
     if os.path.isfile(outname):
         print('remove file '+outname)
         os.remove(outname)
-    iraf.scopy(input = fn, output = outname, w1 = w1, w2 = w2, \
-        apertures = nax2, bands = 1, beams = '', apmodulus = 0, \
-        format = 'multispec', renumber = 'Yes', offset = 0, \
-        clobber = 'No', merge = 'No', rebin = 'No', verbose = 'Yes')
+    iraf.scopy(input=fn, output=outname, w1=w1, w2=w2, apertures=nax2,
+               bands=1, beams='', apmodulus=0, format='multispec',
+               renumber='Yes', offset=0, clobber='No', merge='No',
+               rebin='No', verbose='Yes')
     return outname
+
 
 def icontinuum(fn):
     """
-    call iraf command continuum, if out put file already exist, this function will delete the old one.
+    call iraf command continuum.
+    If out put file already exist, this function will delete the old one.
     fn : fits name
     type : string
     return : out put file name
@@ -58,65 +77,74 @@ def icontinuum(fn):
     if os.path.isfile(outname):
         print('remove file '+outname)
         os.remove(outname)
-    iraf.continuum(input = fn, output = 'c'+fn, lines = '*', bands = 1, \
-        type = 'ratio', replace = 'Yes', wavescale = 'Yes', logscale = 'No', \
-        override = 'No', listonly = 'No', logfiles = 'logfile', \
-        interactive = 'Yes', sample = '*', naverage = 1, function = 'spline3', \
-        order = 1, low_reject = 2.0, high_reject = 0.0, niterate = 10, \
-        grow = 1.0, markrej = 'Yes', graphics = 'stdgraph', cursor = '', \
-        ask = 'Yes')
-    return 'c'+fn # return out fit name
+    iraf.continuum(input=fn, output=outname, lines='*', bands=1, type='ratio',
+                   replace='no', wavescale='Yes', logscale='No', override='No',
+                   listonly='No', logfiles='logfile', interactive='Yes',
+                   sample='*', naverage=1, function='spline3', order=1,
+                   low_reject=1.5, high_reject=0.0, niterate=20, grow=2.0,
+                   markrej='Yes', graphics='stdgraph', cursor='', ask='yes')
+    return outname  # return out fit name
 
-def scombine(fstr, oname, combine = 'sum'):
+
+def scombine(fstr, oname, combine='sum'):
     """
-    call iraf command scombine, generate a combined spectrum file, if the file already exist, this function will delete the old one.
+    Call iraf command scombine, generate a combined spectrum file.
+    If the file already exist, this function will delete the old one.
     fstr : input string, like fstr=abc.fits,abd.fits,abe.fits
     type : string
     oname : output file name
     type : string
-    combine : the spectrum combine method, the method include 'average', 'median', 'sum', default='sum'
+    combine : the spectrum combine method, the method include
+    'average', 'median', 'sum', default='sum'.
     type : string
     """
     if os.path.isfile(oname):
         print('remove file '+oname)
         os.remove(oname)
-    iraf.scombine(input=fstr, output=oname, noutput = '', logfile='STDOUT', \
-        apertures = '', group = 'apertures', combine=combine, reject = 'none',\
-        first = 'No', w1 = 'INDEF', w2 = 'INDEF', dw = 'INDEF', nw = 'INDEF', \
-        log = 'No', scale = 'none', zero = 'none', weight = 'none', \
-        sample = '', lthreshold = 'INDEF', lthreshold = 'INDEF', nlow = 1, \
-        nhigh = 1, nkeep = 1, mclip = 'Yes', lsigma = 3.0, hsigma = 3.0, \
-        rdnoise = 'RDNOISE', gain = 'GAIN', snoise = 0.0, sigscale = 0.1, \
-        pclip = -0.5, grow = 0, blank = 1.0)
+    iraf.scombine(input=fstr, output=oname, noutput='', logfile='STDOUT',
+                  apertures='', group='apertures', combine=combine,
+                  reject='none', first='No', w1='INDEF', w2='INDEF',
+                  dw='INDEF', nw='INDEF', log='No', scale='none', zero='none',
+                  weight='none', sample='', lthreshold='INDEF',
+                  hthreshold='INDEF', nlow=1, nhigh=1, nkeep=1, mclip='Yes',
+                  lsigma=3.0, hsigma=3.0, rdnoise='RDNOISE', gain='GAIN',
+                  snoise=0.0, sigscale=0.1, pclip=-0.5, grow=0, blank=1.0)
+
 
 def gen_cal(fn):
     """
-    generate the calibration file of iraf command telluric, if the file already exist, this function will delete the old one.
+    generate the calibration file of iraf command telluric.
+    If the file already exist, this function will delete the old one.
     fn : fits name
     type : name
     return : out put file name
     type : string
     """
-    windows = [[6800.0, 7110.0], [7110.0, 7470.0], [7500.0, 7800], [8000.0, 8480.0]]
+    windows = [[6800.0, 7108.0],
+               [7111.0, 7470.0],
+               [7500.0, 7800.0],
+               [8000.0, 8480.0]]
     oname = 'cont_' + fn
     if os.path.isfile(oname):
         print('remove file '+oname)
         os.remove(oname)
     namelst = []
     for window in windows:
-        name = scopy_cmp(fn, w1 = window[0], w2 = window[1])
+        name = scopy_cmp(fn, w1=window[0], w2=window[1])
         namelst.append(name)
     namestr = ''
     for name in namelst:
-        oname = icontinuum(name)
-        namestr += oname + ','
+        tmpname = icontinuum(name)
+        namestr += tmpname + ','
     namestr = namestr[:-1]
-    scombine(fstr = namestr, oname=oname)
+    scombine(fstr=namestr, oname=oname)
     return oname
+
 
 def telluric(iname, oname, cal):
     """
-    call iraf command telluric, if output file already exist, this function will delete the old one.
+    call iraf command telluric.
+    if output file already exist, this function will delete the old one.
     iname : input file name
     type : string
     oname : output file name
@@ -124,16 +152,31 @@ def telluric(iname, oname, cal):
     cal : calibration file name
     type : string
     """
-    iraf.telluric(input=iname, output=oname, cal=cal, ignoreaps='Yes', \
-        xcorr = 'Yes', tweakrms='Yes', interactive='Yes', sample='*', \
-        threshold=0.0, lag=10, shift=0.0, scale=1.0, dshift=1.0, dscale=0.2, \
-        offset=1.0, smooth=1, cursor='', airmass='AIRMASS', answer='yes')
+    if os.path.isfile(oname):
+        print('remove file '+oname)
+        os.remove(oname)
+    iraf.telluric(input=iname, output=oname, cal=cal, ignoreaps='Yes',
+                  xcorr='Yes', tweakrms='Yes', interactive='Yes', sample='*',
+                  threshold=0.0, lag=10, shift=0.0, scale=1.0, dshift=1.0,
+                  dscale=0.2, offset=1.0, smooth=1, cursor='', airmass='',
+                  answer='yes')
+
 
 def main():
-    stdlst = open('std.lst').readlines()
-    stdlst = ['waftbo'+i.strip() for i in stdlst]
-    objlst = open('cor_std.lst').readlines()
-    objlst = ['waftbo'+i.strip() for i in objlst]
+    """
+    main function
+    """
+    # stdlst = open('std.lst').readlines()
+    # stdlst = ['awftbo'+i.strip() for i in stdlst]
+    # for name in stdlst:
+    #     print name
+    # objlst = open('cor_std.lst').readlines()
+    # objlst = ['awftbo'+i.strip() for i in objlst]
+    namelst = glob.glob('awftbo*.fits')
+    stdlst = [i for i in namelst if func.is_std(i)]
+    objlst = list(set(namelst) - set(stdlst))
+    for name in objlst:
+        print name
     calstdlst = []
     for name in stdlst:
         calname = gen_cal(name)
@@ -151,6 +194,7 @@ def main():
     scombine(namestr, stdcalname, combine='average')
     for i, name in enumerate(objlst):
         telluric(name, 'telstd_'+name, stdcalname)
+
 
 if __name__ == '__main__':
     main()
