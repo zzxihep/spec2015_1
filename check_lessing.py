de4e6c6dd5115859b1e5ac202e6e1a655e528af2
zzxihep <zhangzx@ihep.ac.cn>
2017-06-27 09:03:26 +0800

    update

--- check_lessing.py
+++ check_lessing.py
@@ -1,73 +1,78 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, shutil
+"""
+check the missing files.
+if some file missing, we can\'t complete the whole process.
+this routine check if there are missing files, and copy missing
+data from other dir.
+"""
+
+import os
 import gen_lst
-import glob
+import func
 
-def find_lst(dirname,filename):
-    path = os.getcwd()
-    upper, spec = os.path.split(path)
-    upper, curdate = os.path.split(upper)
-    other_dir = os.listdir(upper)
-    other_dir = [i for i in other_dir if os.path.isdir(upper + os.sep + i) and i < curdate]
-    other_dir.sort(reverse = True)
-    for i in other_dir:
-        if os.path.isfile(upper + os.sep + i + os.sep + spec + os.sep \
-                + dirname + os.sep + filename):
-            return upper + os.sep + i + os.sep + spec + os.sep + dirname + os.sep + filename
-        hope = upper + os.sep + i + os.sep + spec + os.sep + 'other' + os.sep + dirname + os.sep
-        gen_lst.gen_otherlst(hope)
-        if os.path.isfile(hope + filename):
-            print('find ' + filename + ' in ' + hope)
-            return hope + filename
-    return ''
 
-def copy_lst_and_fit(lst_path, topath):
-    l = open(lst_path).readlines()
-    l = [i.split('\n')[0] for i in l]
-    frompath, lstname = os.path.split(lst_path)
-    print('copy ' + lstname + ' from ' + frompath + ' to ' + topath)
-    shutil.copyfile(frompath + os.sep + lstname, topath + os.sep + lstname)
-    for name in l:
-        print('copy ' + i + ' from ' + frompath + ' to ' + topath)
-        shutil.copyfile(frompath + os.sep + name, topath + os.sep + name)
+def find_lst(lfname):
+    """
+    Find lst file in other date. lfname(lst file name) should be like
+    'Grism_3_Slit_5.05/halogen.lst', 'bias/spec_bias.lst' etc.
+    Assume curent dir = spec.
+    This function assume date dir like '20170312'.
+    lfname : lst file name
+    type : string
+    return : near date same name lst file(abs file path),
+        if can\'t find, return None
+    type : string or None
+    """
+    curdir = os.getcwd()  # current dir, shpuld **/20xxxxxx/spec
+    specdir = os.path.basename(curdir)  # should spec
+    abstoday_dir = os.path.dirname(curdir)  # should **/20xxxxxx
+    today_dir = os.path.basename(abstoday_dir)  # should 20xxxxxx
+    root_dir = os.path.dirname(today_dir)  # should **/
+    oday_dirlst = os.listdir(root_dir)
+    oday_dirlst = [i for i in oday_dirlst if os.path.isdir(i)]
+    oday_dirlst.sort(key=lambda x: abs(int(today_dir)-int(x)))
+    # sort date dir, key is abs diff to current date dir
+    for dirname in oday_dirlst:
+        filepath = root_dir+os.sep+dirname+os.sep+specdir+os.sep+lfname
+        if os.path.isfile(filepath):
+            return filepath
+    return None
 
-def check_bias():
-    if not os.path.isdir('bias'):
-        os.mkdir('bias')
-    if not os.path.isfile(os.getcwd() + os.sep + 'bias' + os.sep + 'spec_bias.lst'):
-        print('lack bias in ' + os.getcwd())
-        lstpath = find_lst('bias','spec_bias.lst')
-        if lstpath == '':
-            print('cann\'t find spec_bias.lst')
-            os._exit(1)
-        else:
-            copy_lst_and_fit(lstpath, os.getcwd() + os.sep + 'bias' + os.sep)
-
-def check_other(dirname):
-    lstname = glob.glob(os.getcwd() + os.sep + dirname + os.sep + '*.lst')
-    stdlstname = ['halogen.lst', 'lamp.lst', 'std.lst']
-    ret = False
-    for i in stdlstname:
-        if i not in lstname:
-            lstpath = find_lst(dirname, i)
-            if lstpath == '':
-                print('cann\'t find ' + dirname + os.sep + i + ' in other place \n abort')
-                os._exit(1)
-            else:
-                copy_lst_and_fit(lstpath, os.getcwd() + os.sep + dirname + os.sep)
-                ret = True
-    return ret
 
 def main():
-    check_bias()
-    dirname = os.listdir(os.getcwd())
-    dirname = [i for i in dirname if os.path.isdir(i) and 'bias' not in i and 'other' not in i]
-    for i in dirname:
-        flag = check_other(i)
-        if flag:
-            gen_lst.main()
+    """
+    Assume current dir = spec
+    check lst file list:
+        bias.lst
+        halogen.lst
+        lamp.lst
+        std.lst
+    """
+    lflst = ['halogen.lst', 'lamp.lst', 'std.lst']
+    curdir = os.getcwd()
+    print 'current dir = ' + curdir
+
+    if not os.path.isdir('bias'):
+        print 'mkdir bias'
+        os.mkdir('bias')
+    bias_name = 'bias/spec_bias.lst'
+    if not os.path.isfile(bias_name):
+        pathname = find_lst(bias_name)
+        func.copy_lstfile(pathname, 'bias')
+
+    dirlst = os.listdir('.')
+    dirlst = [i for i in dirlst if os.path.isdir(i) and
+              i != 'bias' and i != 'other']
+    for dirname in dirlst:
+        for lstname in lflst:
+            dirlstname = dirname+os.sep+lstname
+            if not os.path.isfile(dirlstname):
+                pathname = find_lst(dirlstname)
+                func.copy_lstfile(pathname, dirname)
+    gen_lst.main()
+
 
 if __name__ == '__main__':
     main()
