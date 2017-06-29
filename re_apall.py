e9cb416c35000b1bdcbe1572e8a27f3b4788dacd
zzxihep <zhangzx@ihep.ac.cn>
2017-06-30 03:24:59 +0800

    update

--- re_apall.py
+++ re_apall.py
@@ -11,25 +11,25 @@ from pyraf import iraf
 import sspecplot
 import func
 
+
 def read_conf():
-    path = os.path.split(os.path.realpath(__file__))[0]
-    l = open(path+os.sep+'aperture.lst').readlines()
-    l = [i.split() for i in l]
-    l = [[i[0], [float(i[1]), float(i[2]), i[3]]] for i in l]
-    l = dict(l)
-    return l
+    path = func.script_path
+    lst = open(path+os.sep+'aperture.lst').readlines()
+    lst = [i.split() for i in lst]
+    lst = [[i[0], [float(i[1]), float(i[2]), i[3]]] for i in lst]
+    lst = dict(lst)
+    return lst
+
 
 def apall(lstfile):
     iraf.noao()
     iraf.twodspec()
-    iraf.apextract(dispaxis = 2, database = 'database')
-    f = open(lstfile)
-    l = f.readlines()
-    f.close()
-    l = [tmp.split('\n')[0] for tmp in l]
+    iraf.apextract(dispaxis=2, database='database')
+    lst = open(lstfile)
+    lst = [tmp.strip() for tmp in lst]
     path = os.getcwd()
     confdic = read_conf()
-    for i in l:
+    for i in lst:
         infile = 'wftbo' + i
         outfile = 'awftbo' + i
         laper = -15.0
@@ -53,38 +53,38 @@ def apall(lstfile):
             if os.path.isfile(delfile):
                 print('remove ' + delfile)
                 os.remove(delfile)
-            iraf.apall(input = infile
-                , output = outfile, apertures = 2, format = 'multispec'
-                , references = '', profiles = '', interactive = True
-                , find = True, recenter = True, resize = False
-                , edit = True, trace = True, fittrace = True
-                , extract = True, extras = True, review = True
-                , line = 'INDEF', nsum = 10, lower = laper
-                , upper = raper, apidtable = '', b_function = 'chebyshev'
-                , b_order = 2, b_sample = back_samp, b_naverage = -25
-                , b_niterate = 1, b_low_reject = 3.0, b_high_reject = 3.0
-                , b_grow = 0.0, width = 5.0, radius = 10.0
-                , threshold = 0.0, nfind = 2, minsep = 5.0
-                , maxsep = 100000.0, order = 'increasing', aprecenter = ''
-                , npeaks = 'INDEF', shift = True, llimit ='INDEF'
-                , ulimit = 'INDEF', ylevel = 0.1, peak = True
-                , bkg = True, r_grow = 0.0, avglimits = False
-                , t_nsum = 20, t_step = 10, t_nlost = 3, t_function = 'legendre'
-                , t_order = 7, t_sample = '*', t_naverage = 1
-                , t_niterate = 1, t_low_reject = 3.0, t_high_reject = 3.0
-                , t_grow = 0.0, background = 'median', skybox = 1
-                , weights = 'none', pfit = 'fit1d', clean = True
-                , saturation = 'INDEF', readnoise = 9.4, gain = 0.35
-                , lsigma = 4.0, usigma = 4.0, nsubaps = 1)
+            iraf.apall(input=infile, output=outfile, apertures=2,
+                       format='multispec', references='', profiles='',
+                       interactive=True, find=True, recenter=True,
+                       resize=False, edit=True, trace=True, fittrace=True,
+                       extract=True, extras=True, review=True, line='INDEF',
+                       nsum=10, lower=laper, upper=raper, apidtable='',
+                       b_function='chebyshev', b_order=2, b_sample=back_samp,
+                       b_naverage=-25, b_niterate=1, b_low_reject=3.0,
+                       b_high_reject=3.0, b_grow=0.0, width=5.0, radius=10.0,
+                       threshold=0.0, nfind=2, minsep=5.0, maxsep=100000.0,
+                       order='increasing', aprecenter='', npeaks='INDEF',
+                       shift=True, llimit='INDEF', ulimit='INDEF', ylevel=0.1,
+                       peak=True, bkg=True, r_grow=0.0, avglimits=False,
+                       t_nsum=20, t_step=10, t_nlost=3, t_function='legendre',
+                       t_order=7, t_sample='*', t_naverage=1, t_niterate=1,
+                       t_low_reject=3.0, t_high_reject=3.0, t_grow=0.0,
+                       background='median', skybox=1, weights='none',
+                       pfit='fit1d', clean=True, saturation='INDEF',
+                       readnoise=9.4, gain=0.35, lsigma=4.0, usigma=4.0,
+                       nsubaps=1)
             iraf.flpr()
             sspecplot.sspecplot(outfile)
-            getval = raw_input('Are you need repeat apall,may be clean should be close(r/n)')
+            getval = raw_input(('Are you need repeat apall,'
+                                'may be clean should be close(r/n)'))
             if getval != 'r':
                 break
+
 
 def main():
     print('='*20 + ' extract ' + '='*20)
     apall('cor_lamp.lst')
+
 
 if __name__ == '__main__':
     main()
