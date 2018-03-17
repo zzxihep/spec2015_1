91b0f7c5a2c075b499cf59306776ff2edc5ab0a8
zzxihep <zhangzx@ihep.ac.cn>
2018-03-17 15:26:03 +0800

    correct script name mistake

--- spec2015.sh
+++ spec2015.sh
@@ -13,7 +13,7 @@ cd spec
 ${0%/*}/classify_fits.py
 ${0%/*}/mv_to_other.py
 ${0%/*}/gen_lst.py
-${0%/*}/check_lessing.py
+${0%/*}/check_missing.py
 ${0%/*}/check_bad.py
 ${0%/*}/check_manual.py
 ${0%/*}/gen_zero.py
